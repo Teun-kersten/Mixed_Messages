1576a08dd18d7d457229851161d48e2a0aba1e41
Teun <tlj.programming@gmail.com>
2026-05-05 16:16:40 +0200

    added characters and action arrays to the file

---
+++ index.js
@@ -0,0 +1,12 @@
+/*for this project the challenge was to create random mixed messages function that uses at least three different 
+ pieces of data. I will be using six different data sets spread out into multiple arrays. */
+
+ const character = ['knight', 'ghost', 'wizard', 'pirate', 'alien', 'gnome', 'forest critter', 'dragon', 'robot',
+     'rogue']
+
+     const action = ['attacks', 'invites you in their order', 'haunts you', 'scares you',
+     'tries to turn you into a strawberry', 'offers you a potion', 'plunders your booty', 'makes you walk the plank',
+      'tries to abduct you', 'invites you into the galactic order', 'tries to lure you into the forest',
+       'offers you a magic mushroom', 'tries to gnaw at your ankles', 'offers you a quest', 'spews fire at you', 
+       'tries to pick you up and take to the skies', 'tries to disassemble you', 'tries to clean you up', 
+       'tries to take your coin purse', 'fires an arrow from afar']
