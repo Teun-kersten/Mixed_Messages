58b55def01fb8b732572d7f4348274e9ffdef00e
Teun <tlj.programming@gmail.com>
2026-05-07 11:48:18 +0200

    finale has been updates to hold the different datasets organised by the type of outcome, increasing readibility and making it easier to generate the final result. rollType has also been added to determine the type of outcome based on the die roll, which is then used to choose the appropriate message

--- index.js
+++ index.js
@@ -22,33 +22,164 @@ const result = ['a wave of stagefright washes over you and you freeze in place',
 'you offer them your coin purse, which is enough for them to leave you be', 'You make them an offer they can\'t refuse',
 'you turn around but trip over your own shoelaces', 'you try to run but aren\'t fast enough', 'you quickly make your escape', 'you dissapear in a cloud of smoke'];
 // the sixth and final dataset consists of the wrap up message that concludes the adventure
-const finale = ['The knight shakes his head, not even bothering drawing his sword, the damage has already been done',
-    'the knight defeats you easily, leaving you heavily wounded', 'the knight fights with all his might, but is unable to defeat you',
-    'humiliated, the knight takes off his armor and lays down in a foetal position, crying',
-    'not only does the ghost posess you, it makes you do a silly dance to add insult to injury',
-    'you try to resist, but the spirit is too strong, you are now posessed', 'you manage to fight off the ghost, and it passes to the other side',
-    'The ghost is so impressed by your resistance it decides to become your spectral companion, helping you on your adventures',
-    'the wizard turns you into a strawberry and puts you in his pocket, muttering something about fruit salads',
-    'the wizard turns you into a strawberry, thankfully the spell wears off after a few minutes, but you\'ll never be the same',
-    'you counter the spell, sending it back to the wizard, who narrowly dodges it', 'you send the spell directly back to the wizard, turning him into a strawberry instead. You victoriously dangle hi over your open mouth',
-    'the pirate flips up their eyepatch and gives you a judgemental look, they do\'t even want the booty anymore',
-    'the pirate swashbuckles you thoroughly, consider your booty plundered!', 'the pirate is bested and walks the plank in shame',
-    'the pirate looks up to the sky as he realises the real booty were the friends we make along the way',
-    'the alien tractor beams you up to their spacecraft, where they proceed to probe you', 'the alien abducts you, but rather than probing you they dress you up and keep you as their pet',
-    'you resist the alien\'s abduction and they decide to leave you and earth alone', 'the alien is so impressed by you that he invites you into the galactic order',
-    'the gnome doesn\'t want to waste one of their magic mushrooms on you, and just gives you a regular one, they walk away dissapointed',
-    'the gnome fiendishly gives you a poisonous mushroom, giving you a serious tummy ache',
-    'the gnome gives you a magic mushroom, which heals you up a little', 'the gnome gives you a magic mushroom, you grow to twice your size and feel a unnatural urge to save a princess',
-    'the forest critter takes one nibble of your ankle, you see it\'s face turn sour as it looks up at you in disgust', 'the forest critter gnaws at your ankles, ruining your shoes and giving you fleas',
-    'you send the fores critter flying, they scurry off into the woods', 'the forest critter turns out to be a forest deity in disguise, they are impressed with you and share the secrets of the earth with you, granting you the power to control plants',
-    'the dragon spews a single spark at you, singing your hair and rendering you bald', 'the dragon spews a might flame at you, burning you to a crisp',
-    'you evade the dragon\'s flames and strike back, it flies away in defeat', 'the dragon is so impressed it decides to take you to its hoard, allowing you to choose a single prize',
-    'instead of dissassembling you, the robot reassembles you, placing your left foor on top of your head',
-    'the robot dissassembles you, leaving you, quite literally, in pieces', 'you manage to outsmart the robot by swiftly turning it off',
-    'the robot suffers the ultimate humiliation as it is reassembled into a roomba', 'the rogue steals your coin purse, which he flings at the back of your head a moment later after finding out it\'s empty',
-    'the rogue steals your coin purse, leaving you pennyless', 'the rogue fumbles around your waist, but your fannypack proves too secure, and he scurries away',
-    'you outsmart the rogue and steal his coin purse instead, which is full of gold coins'
-];
+const finale = {
+  knight: {
+    critFail: [
+      'The knight shakes his head, not even bothering drawing his sword, the damage has already been done'
+    ],
+    fail: [
+      'the knight defeats you easily, leaving you heavily wounded'
+    ],
+    success: [
+      'the knight fights with all his might, but is unable to defeat you'
+    ],
+    nat20: [
+      'humiliated, the knight takes off his armor and lays down in a foetal position, crying'
+    ]
+  },
+
+  ghost: {
+    critFail: [
+      'not only does the ghost posess you, it makes you do a silly dance to add insult to injury'
+    ],
+    fail: [
+      'you try to resist, but the spirit is too strong, you are now posessed'
+    ],
+    success: [
+      'you manage to fight off the ghost, and it passes to the other side'
+    ],
+    nat20: [
+      'The ghost is so impressed by your resistance it decides to become your spectral companion, helping you on your adventures'
+    ]
+  },
+
+  wizard: {
+    critFail: [
+      'the wizard turns you into a strawberry and puts you in his pocket, muttering something about fruit salads'
+    ],
+    fail: [
+      'the wizard turns you into a strawberry, thankfully the spell wears off after a few minutes, but you\'ll never be the same'
+    ],
+    success: [
+      'you counter the spell, sending it back to the wizard, who narrowly dodges it'
+    ],
+    nat20: [
+      'you send the spell directly back to the wizard, turning him into a strawberry instead. You victoriously dangle him over your open mouth'
+    ]
+  },
+
+  pirate: {
+    critFail: [
+      'the pirate flips up their eyepatch and gives you a judgemental look, they don\'t even want the booty anymore'
+    ],
+    fail: [
+      'the pirate swashbuckles you thoroughly, consider your booty plundered!'
+    ],
+    success: [
+      'the pirate is bested and walks the plank in shame'
+    ],
+    nat20: [
+      'the pirate looks up to the sky as he realises the real booty were the friends we make along the way'
+    ]
+  },
+
+  alien: {
+    critFail: [
+      'the alien tractor beams you up to their spacecraft, where they proceed to probe you'
+    ],
+    fail: [
+      'the alien abducts you, but rather than probing you they dress you up and keep you as their pet'
+    ],
+    success: [
+      'you resist the alien\'s abduction and they decide to leave you and earth alone'
+    ],
+    nat20: [
+      'the alien is so impressed by you that he invites you into the galactic order'
+    ]
+  },
+
+  gnome: {
+    critFail: [
+      'the gnome doesn\'t want to waste one of their magic mushrooms on you, and just gives you a regular one, they walk away dissapointed'
+    ],
+    fail: [
+      'the gnome fiendishly gives you a poisonous mushroom, giving you a serious tummy ache'
+    ],
+    success: [
+      'the gnome gives you a magic mushroom, which heals you up a little'
+    ],
+    nat20: [
+      'the gnome gives you a magic mushroom, you grow to twice your size and feel an unnatural urge to save a princess'
+    ]
+  },
+
+  'forest critter': {
+    critFail: [
+      'the forest critter takes one nibble of your ankle, you see its face turn sour as it looks up at you in disgust'
+    ],
+    fail: [
+      'the forest critter gnaws at your ankles, ruining your shoes and giving you fleas'
+    ],
+    success: [
+      'you send the forest critter flying, they scurry off into the woods'
+    ],
+    nat20: [
+      'the forest critter turns out to be a forest deity in disguise, they are impressed with you and share the secrets of the earth with you, granting you the power to control plants'
+    ]
+  },
+
+  dragon: {
+    critFail: [
+      'the dragon spews a single spark at you, singing your hair and rendering you bald'
+    ],
+    fail: [
+      'the dragon spews a mighty flame at you, burning you to a crisp'
+    ],
+    success: [
+      'you evade the dragon\'s flames and strike back, it flies away in defeat'
+    ],
+    nat20: [
+      'the dragon is so impressed it decides to take you to its hoard, allowing you to choose a single prize'
+    ]
+  },
+
+  robot: {
+    critFail: [
+      'instead of dissassembling you, the robot reassembles you, placing your left foot on top of your head'
+    ],
+    fail: [
+      'the robot dissassembles you, leaving you, quite literally, in pieces'
+    ],
+    success: [
+      'you manage to outsmart the robot by swiftly turning it off'
+    ],
+    nat20: [
+      'the robot suffers the ultimate humiliation as it is reassembled into a roomba'
+    ]
+  },
+
+  rogue: {
+    critFail: [
+      'the rogue steals your coin purse, which he flings at the back of your head a moment later after finding out it\'s empty'
+    ],
+    fail: [
+      'the rogue steals your coin purse, leaving you pennyless'
+    ],
+    success: [
+      'the rogue fumbles around your waist, but your fannypack proves too secure, and he scurries away'
+    ],
+    nat20: [
+      'you outsmart the rogue and steal his coin purse instead, which is full of gold coins'
+    ]
+  }
+};
+// this function will decide what outcome to choose based on the die roll
+const rollType = (roll) => {
+    if (roll === 1) { return 'critFail'; }
+    if (roll >= 2 && roll <= 11) { return 'fail'; }
+    if (roll >= 12 && roll <= 19) { return 'success'; }
+    if (roll === 20) { return 'nat20'; }
+}
 // to make it easer to generate the outcomes we use a function that will pick an outcome from each dataset and can be used multiple times
 const randomPicker = arr => arr[Math.floor(Math.random() * arr.length)];
 // this function will generate the random message by picking a random element from each dataset and combining them into one string
